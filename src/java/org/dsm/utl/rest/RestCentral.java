98a542c7292ec45102b805f2a46bfd3b06edb081
Confleis <blackstar371@outlook.com>
2023-11-01 20:35:02 -0600

    Se agrego la carpeta de servicios

---
+++ src/java/org/dsm/utl/rest/RestCentral.java
@@ -0,0 +1,14 @@
+package org.dsm.utl.rest;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Black
+ */
+public class RestCentral {
+    
+}
