134b132d30804d2dd6c937834c18a9beab60e6a3
JuanPablo01 <blackstar371@outlook.com>
2023-11-04 00:31:10 -0600

    Se agrego el card de los productos funcional

--- web/Js/myScript.js
+++ web/Js/myScript.js
@@ -1,30 +1,56 @@
 let productsInfo = [];
 
-
-
 /// Carga del contenido de productos
-function loadProducts(data) {
+async function loadProducts() {
+    await getJsonData();
+
     let position = 0;
+    let max = productsInfo.length;
     let buttonPrevius = document.getElementById('btn-previous');
     let buttonNext = document.getElementById('btn-next');
 
+    insertDataContainer(position);
+
     buttonNext.addEventListener("click", () =>{
+        if(position == max - 1) {
+            position = 0;
+        } else {
+            position++;
+        }
+        insertDataContainer(position);
     });
 
     buttonPrevius.addEventListener("click", () => {
-        alert("Presionarte el boton anterior");
+        if(position == 0) {
+            position = max - 1;
+        } else {
+            position--;
+        }
+        insertDataContainer(position);
     });
-
-    console.log(data);
 }
 
-function changeProduct(index) {
+function insertDataContainer(index) {
+    let title = document.getElementById('lb-title');
+    let type = document.getElementById("lb-type");
+    let content = document.getElementById("lb-content");
+    let description = document.getElementById("lb-description");
 
+    title.textContent = productsInfo[index].title;
+    type.textContent = productsInfo[index].type;
+    content.textContent = productsInfo[index].content;
+    description.textContent = productsInfo[index].description;
+}
+
+function getJsonData() {
+    return fetch('Json/medsInfo.json')
+    .then(res => res.json())
+    .then((data) => {
+        productsInfo = data;
+    });
 }
 
 /// Carga del DOM
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('Json/medsInfo.json')
-    .then(res => res.json())
-    .then(data => loadProducts(data));
+    loadProducts();
 });
