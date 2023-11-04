bfce3a53c419bce3a27f8981c2cc28df8e975453
Joel Briones <joellbriones701@gmail.com>
2023-11-03 22:18:40 -0600

    Login

--- web/Js/MyScriptIndex.js
+++ web/Js/MyScriptIndex.js
@@ -1,10 +1,27 @@
+loadIndex();
+
+function loadIndex(){
+    fetch("./Modules/Index/index.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("main-container").innerHTML = data;
+    })
+}
+
 // agregar login a el main
 function login(url) {
 
     fetch(url)
-    .then(response =>response.text())
-    .then(data => {
-        document.getElementById("main-container").innerHTML = data;
-    });
-    
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("main-container").innerHTML = data;
+        });
+
+}
+ function regresarIndex(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("main-container").innerHTML = data
+        })
 }
