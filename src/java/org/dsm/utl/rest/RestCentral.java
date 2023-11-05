083ac8f9a49621283237928e647e3671afefa4f6
JuanPablo01 <blackstar371@outlook.com>
2023-11-05 02:23:26 -0600

    Se agregaron las librerias para el REST

--- src/java/org/dsm/utl/rest/RestCentral.java
+++ src/java/org/dsm/utl/rest/RestCentral.java
@@ -1,13 +1,10 @@
 package org.dsm.utl.rest;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author Black
+ *Created by DreamSoft
+ * Juan Pablo Perez Fernandez
+ * Alejandra Hernandez Sauceda
+ * Joel Ezequiel Rodrigues Briones
+ * Edgar Castillo Aguas
  */
 public class RestCentral {
     
